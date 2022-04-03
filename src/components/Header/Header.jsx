d921365c62b8373b11417981e933c6b04d2cc464
RinaOcean <antyfifa@gmail.com>
2022-04-03 17:01:47 +0300

    add main section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,27 @@
+import Container from '../Container'
 import styles from './Header.module.css'
+
 
 const Header = () => {
   return (
+    <Container>
     <div className={styles.header}>
       <div className={styles.svitloDarLogo}>
         <div>
          <span className={styles.svitlo}>svitlo</span>
          <span className={styles.dar}>dar</span>
         </div>
-        <span className={styles.charityFund}>charity fund</span>
+        {/* <span className={styles.charityFund}>charity fund</span> */}
       </div>
 
+      <nav className={styles.headerNav}>
+        <span className={styles.headerNavItem}>about us</span>
+        <span className={styles.headerNavItem}>what we do</span>
+        <span className={styles.headerNavItem}>contacts</span>
+      </nav>
+
     </div>
+    </Container>
   )
 }
 
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,12 +1,15 @@
 import styles from './Main.module.css'
 import ybHeart from '../../assets/yellowBlueHeart.svg'
+import Container from '../Container'
 
 const Main = () => {
   return (
-    <div className={styles.main}>
-     <h1 className={styles.mainHeader}>Charity Fund Svitlodar</h1>
-     <img src={ybHeart} alt='yellow-blue heart'/>
-    </div>
+    <Container>
+      <section className={styles.main}>
+        <h1 className={styles.mainHeader}>Charity Fund Svitlodar</h1>
+        <img className={styles.heartImg} src={ybHeart} alt='yellow-blue heart'/>
+      </section>
+    </Container>
   )
 }
 
