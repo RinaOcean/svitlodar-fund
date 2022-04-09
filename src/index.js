ebb0921f79a59d53efa4951c1b5de0c0bf0966f9
RinaOcean <antyfifa@gmail.com>
2022-04-09 12:45:15 +0300

    styles

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './i18n';
 
 const rootElement = document.getElementById('root');
