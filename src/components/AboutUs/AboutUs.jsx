b94e6266733a78c4a32e738e5ec93b15f7b5be51
RinaOcean <antyfifa@gmail.com>
2022-04-13 18:36:37 +0300

    addtranslation switcher

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -2,9 +2,15 @@ import Container from '../Container'
 import styles from './AboutUs.module.css'
 import yellowEclps from '../../assets/yellowEllipse.svg'
 import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router';
 
 const AboutUs = () => {
   const { t } = useTranslation();
+
+  let navigate = useNavigate()
+  function onClickHandler() {
+   navigate("/aboutus")
+  }
 
   return (
     <section id="about" className={styles.aboutUs}>
@@ -12,10 +18,11 @@ const AboutUs = () => {
         <h1 className={styles.headding}>{t('about us')}</h1>
         <div className={styles.imgWrapper}>
           <p className={styles.description}>
-          {t('Charity Fund Svitlodar was founded on October 01, 2015. We started as volunteers helping the victims of the anti-terrorist operation (ATO) in the East of Ukraine in 2014-2015.')}
+          {t('Charity Fund Svitlodar was founded on October 1, 2015. We started as volunteers helping the victims of the anti-terrorist operation (ATO) in the East of Ukraine in 2014.')}
           </p>
           <img className={styles.eclipseOneImg} src={yellowEclps} alt='yellow-blue heart'/>
         </div>
+        <button type="button" className={styles.detailButton} onClick={onClickHandler}>{t('Details')}</button>
       </Container>
     </section>
     
