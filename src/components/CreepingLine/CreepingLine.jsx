a52bc465aad1bcf38282a8600fd09e4b075074fa
RinaOcean <antyfifa@gmail.com>
2022-05-07 19:59:53 +0300

    styles fixes

--- src/components/CreepingLine/CreepingLine.jsx
+++ src/components/CreepingLine/CreepingLine.jsx
@@ -2,16 +2,17 @@ import { useTranslation } from "react-i18next"
 import Container from "../Container"
 import Marquee from "react-fast-marquee";
 import styles from './CreepingLine.module.css';
+import ybHeart from '../../assets/yellowBlueHeart.svg'
 
 const CreepingLine = () => {
   const {t} = useTranslation()
 
-  return (
-    // <Container>
-      <Marquee className={styles.creepingLine}>
-         <span>{t('A week ago, we received 4 tons of humanitarian aid from Germany - medicines, food, consumables for hospitals and hospitals in Kharkiv, and very touching gifts from children for children. ')}</span>
-      </Marquee>
-    // </Container>
+  return (    
+      <Marquee className={styles.creepingLine} speed={40} gradientWidth={50}>
+         <img className={styles.heartImg} src={ybHeart} alt='yellow-blue heart'/>
+         <span>{t('A week ago, we received 4 tons of humanitarian aid from Germany - medicines, food, consumables for hospitals in Kharkiv, and very touching gifts from children for children. ')}</span>
+         <img className={styles.heartImg} src={ybHeart} alt='yellow-blue heart'/>
+      </Marquee>    
   )
 }
 
